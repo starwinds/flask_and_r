09e2df6313477cb8a49107e6b2a864a4181b196e
root <root@mongos.cs2462cloud.internal>
2016-01-19 15:42:26 +0900

    some change

--- flaskapp.py
+++ flaskapp.py
@@ -21,7 +21,6 @@ def index():
 
 @app.route('/test/<apikey>/<secret>/<owner>')
 def test(apikey,secret,owner):
-	#return '<h1>Hello %s!, %s^^</h1>' %(apikey,secret) 
 	print "apikey = %s, scret = %s, owner = %s" %(apikey,secret,owner)
 	ucloud_client = Client(api_type='watch',api_key=apikey,secret=secret,owner=owner)
 	response = ucloud_client.request('listMetrics')
@@ -44,10 +43,6 @@ def list_metric():
 @app.route('/getmetricdata')
 
 def get_metric_data():
-	#apikey = str(session['apikey'])
-        #secret = str(session['secret'])
-	#apikey = "jUMEGdE43x_hnj0P3BeICCQ7BFntIokmrOb8tV5B-y0VkSVtOH0EAvtKOEP5G-mMEGSTSy02B44tjAxWwcNEsQ"
-	#secret = "dFy9HakzEPgWSxe0pJN-wbog5K293_un_bsb84cqI17YqWsNsJ0EUQT0qkd6jz9v6zCqKsWiQL2_N_iTq-Ab3Q"
 	apikey = request.args.get('apikey')
 	secret = request.args.get('secret')
 	owner = request.args.get('owner')
@@ -61,8 +56,6 @@ def get_metric_data():
 		input_args["dimensions.member.1.name"] = dimension_name
 	if dimension_value != "no dimension":
 		input_args["dimensions.member.1.value"] = dimension_value
-	#input_args["dimensions.member.1.name"] = request.args.get('dimension_name')
-	#input_args["dimensions.member.1.value"] = request.args.get('dimension_value')
 	input_args["metricname"] = request.args.get("metricname")
 	input_args["starttime"] = request.args.get('starttime')
 	input_args["endtime"] = request.args.get('endtime')
@@ -83,25 +76,14 @@ def get_metric_data():
 
 @app.route('/result')
 def show_result():
-	#arg = session['arg']
-	#arg = request.args['arg']
-	#return '%s' % arg
 	apikey = str(session['apikey'])
 	secret = str(session['secret'])
 	owner = str(session['account'])
 	print 'apikey = %s, secret = %s, account = %s' %(apikey,secret,owner)
 	keyparam = 'apikey=%s&secret=%s&owner=%s' %(apikey,secret,owner)
-#	ucloud_client = Client(api_type='watch',api_key=apikey,secret=secret)
-#	parser = Parse()
-#        response = ucloud_client.request('listMetrics')
-#        dump_result = json.dumps(response)
-#	parsed_result = parser.parse(dump_result)
-	#return '%s' % parsed_result
 	base_url = "http://server_ip:3838/test/?"
-	#request_url = base_url + keyparam + "&param=%s"%parsed_result
 	request_url = base_url + keyparam
 	print "request_url = %s"%request_url
-	#return redirect("http://211.253.8.97:3838/test/?param=%s" % parsed_result)
 	return redirect(request_url)
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -112,25 +94,12 @@ def login():
 	session['secret']=str(request.form['secretkey'])
 	session['account']=str(request.form['account'])
 	print "apikey=%s, secretkey=%s, account=%s" % (request.form['apikey'],request.form['secretkey'],request.form['account'])
-#	ucloud_client = Client(api_type='watch',api_key=str(request.form['apikey']),secret=str(request.form['secretkey']))
-	#ucloud_client = Client(api_type='watch',api_key=session['apikey'],secret=session['secret'])
-	#response = ucloud_client.request('listMetrics')
-	#dump_result = json.dumps(response)
-	#session['arg'] = dump_result
-	#arg = dump_result
-	#time.sleep(1)
-        #print dump_result
-	#return redirect(url_for('.show_result',arg=arg))
 	session.modified = True
 	return redirect(url_for('.show_result'))
     return render_template('login.html', error=error)
 
 @app.route('/logout')
 def logout():
-    #session.clear()
     session.pop('logged_in', None)
     flash('You were logged out')
-   # return redirect(url_for('show_entries'))
-
-if __name__ == '__main__':
     app.run(host='0.0.0.0')
